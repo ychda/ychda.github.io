d58f2255fdbcbca361b2764e5ec0a2dd2c12aae5
ychda <ychda@qq.com>
2026-05-05 22:08:51 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [],
-    base: '/ychda.github.io/',
+    base: '/',
     build: {
         rollupOptions: {
             input: {
